7d919186213242dacef949c7651e12198f307494
pagos21 <paga@localhost.yeah>
2020-09-24 09:44:54 +0200

    v 1.0.2

--- index.php
+++ index.php
@@ -12,26 +12,33 @@ class Square {
   public function perimetro(){
     return 4 * $this-> lato;
   }
-
+  public function str()  {
+    return "L'area è: ". $this -> area(). "<br>"
+    . "Il perimetro è: ". $this -> perimetro(). "<br><br>";
+  }
   public function __toString(){
-     return "L'area è: ". $this -> area(). "<br>"
-     . "Il perimetro è: ". $this -> perimetro(). "<br><br>";
+    return "Square: <br>". $this -> str();
   }
 }
 
-  class Cube extends Square{
-    
-    public function vol(){
-      return parent::area() * $this -> lato;
-    }
-    public function superf(){
-      return 6 * parent::area();
-    }
-    public function __toString(){
-      return "Volume : ". $this -> vol(). "<br>"
-      . "Superficie : ". $this -> superf(). "<br><br>";
-    }
+
+class Cube extends Square{
+
+  public function vol(){
+    return parent::area() * $this -> lato;
   }
+  public function superf(){
+    return 6 * parent::area();
+  }
+  public function str()  {
+  return "Volume : ". $this -> vol(). "<br>"
+  . "Superficie : ". $this -> superf(). "<br><br>";
+
+  }
+  public function __toString(){
+    return "Cube: <br>". $this -> str();
+  }
+}
 
 $square1 = New Square(15);
 $square2 = New Square(5);
